48b4c4b955f81717b1baba8047af56242bdf4e8f
Artur <arlssi@ttu.ee>
2023-11-15 21:54:14 +0300

    .

--- Uno1/ConsoleApp/Program.cs
+++ Uno1/ConsoleApp/Program.cs
@@ -6,22 +6,10 @@ public static class Program
 {
     private static void Main()
     {
-        // var dbFilePath = @"C:\Program Files\SQLiteStudio\UnoDb.db"; // Replace with your actual file path
-         // var connectionString = $"Data Source={dbFilePath};";
-         //
-         // var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
-         //     .UseSqlite(connectionString)
-         //     .EnableDetailedErrors()
-         //     .EnableSensitiveDataLogging()
-         //     .Options;
-         // using var db = new AppDbContext(contextOptions);
-         // db.Database.Migrate();
-         // Console.WriteLine(db);
-         
-         //Start the menu with JSON
+         //Start the menu with JSON repository
          // Menu.Menu.RunMenu(NewOrLoadGame.NewGame, NewOrLoadGame.LoadGameJson);
          
-         //Start the menu with DB
+         //Start the menu with SQLite repository
          Menu.Menu.RunMenu(GameSetupLoader.NewGame, GameSetupLoader.LoadGameDb);
     }
 }
